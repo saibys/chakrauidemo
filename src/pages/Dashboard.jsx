03af9d00912a4f09d23cd150626dce25d1900317
rioxraman <ramanpkgupta@gmail.com>
2023-06-14 12:00:28 +0530

    first commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,23 @@
+import { Heading, Text, Container } from "@chakra-ui/react";
+
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <Container>
+      <Heading my="30px" p="10px">
+        Chakra UI
+      </Heading>
+      <Text ml="20px">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        repellendus officiis labore nulla suscipit similique nisi ea
+        consequuntur impedit! Repellendus, aperiam est dolor consequatur sunt
+        earum dolores totam odit placeat?
+      </Text>
+      <Text marginLeft="20px" color="red.300" fontWeight="bold">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        repellendus officiis labore nulla suscipit similique nisi ea
+        consequuntur impedit! Repellendus, aperiam est dolor consequatur sunt
+        earum dolores totam odit placeat?
+      </Text>
+    </Container>
+  );
 }
